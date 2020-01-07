c9c04371f3c97dae52e72ab9750dc646800d157c
zhangxuelian <761303248@qq.com>
2020-01-07 16:17:59 +0800

    add karma and jasmine

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt){
+    // Load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
+    require('load-grunt-tasks')(grunt);
+    grunt.initConfig({
+        ngversion: '1.2.21',
+        pkg: grunt.file.readJSON('package.json'),
+        dist: 'dist',
+        filename: 'angularjs-xue',
+        banner: [
+            '/**',
+            ' * <%= pkg.name %>',
+            ' * <%= pkg.homepage %>\n',
+            ' * Version: <%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>',
+            ' * License: <%= pkg.license %>',
+            ' */'
+        ].join('\n')
+    });
+    grunt.registerTask('default', []);
+}
