a9c57407ae0c427adcf8815779d27260a1bbb380
liujunlue <liujunlue@gosuncn.com>
2020-01-17 16:00:41 +0800

    ljl: add object

--- src/util/object.js
+++ src/util/object.js
@@ -1,4 +1,37 @@
 angular.module('xue.util.object', [])
     .service('xueUtilObject', [function () {
-
+        /**
+         * json中把空对象移除
+         *
+         * @param {any} json
+         * @returns
+         */
+        this.removeEmptyField = function (json) {
+            var newJson = {};
+            for (var key in json) {
+                if (json[key]) {
+                    newJson[key] = json[key];
+                }
+            }
+            return newJson;
+        };
+        /**
+         * json中把数组的对象中的空属性移除
+         *
+         * @param {any} json
+         * @returns
+         */
+        this.removeEmptyParams = function (array) {
+            var newArray = [];
+            for (var i = 0, len = array.length; i < len; i++) {
+                var newObj = {};
+                for (var j in array[i]) {
+                    if (array[i][j]) {
+                        newObj[j] = array[i][j];
+                    }
+                }
+                newArray.push(newObj);
+            }
+            return newArray;
+        };
     }]);
--- src/util/test/object.spec.js
+++ src/util/test/object.spec.js
@@ -1,3 +1,32 @@
 describe('object util', function () {
-
+    var xueUtilObject;
+    beforeEach(module('xue.util.object'));
+    beforeEach(inject(function(_xueUtilObject_){
+        xueUtilObject = _xueUtilObject_;
+    }));
+    it('检查json中把空对象移除',function(){
+        expect(xueUtilObject.removeEmptyField({
+            name: 'Gosuncn',
+            age: 23,
+            address: ''
+        })).toEqual({
+            name: 'Gosuncn',
+            age: 23
+        });
+    });
+    it('检查json中把数组的对象中的空属性移除',function(){
+        expect(xueUtilObject.removeEmptyParams([{
+            name: 'Gosuncn',
+            age: 23,
+            address: ''
+        }, {
+            phone: '',
+            tel: '13144566987'
+        }])).toEqual([{
+            name: 'Gosuncn',
+            age: 23
+        }, {
+            tel: '13144566987'
+        }]);
+    });
 });
