1d84e08b4d907ed00af34f7a3adf30cf47e03128
huguiqing <huguiqing000@163.com>
2020-01-17 17:48:32 +0800

    Added:增加string功能

--- src/util/test/string.spec.js
+++ src/util/test/string.spec.js
@@ -1,3 +1,7 @@
 describe('string util', function () {
-
+    var xueUtilString;
+    beforeEach(module('xue.util.string'));
+    beforeEach(inject(function(_xueUtilString_){
+        xueUtilString = _xueUtilString_;
+    }));
 });
