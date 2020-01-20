d2b299ab2f68617eb0969fa39658c8d954c1c1b9
zhangxuelian <761303248@qq.com>
2020-01-20 16:11:22 +0800

    feat(misc): add validate git commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -247,7 +247,13 @@ module.exports = function (grunt) {
         }
     };
 
-    grunt.registerTask('default', ['cssmin', 'eslint', 'html2js', 'sass', 'karma', 'build', 'copy']);
+    grunt.registerTask('default', ['enforce', 'cssmin', 'eslint', 'html2js', 'sass', 'karma', 'build', 'copy']);
+    grunt.registerTask('enforce', `Install commit message enforce script if it doesn't exist`, function () {
+        if (!grunt.file.exists('.git/hooks/commit-msg')) {
+            grunt.file.copy('misc/validate-commit-msg.js', '.git/hooks/commit-msg');
+            require('fs').chmodSync('.git/hooks/commit-msg', '0755');
+        }
+    });
     grunt.registerTask('build', 'Create bootstrap build files', function () {
         grunt.file.expand({
             filter: 'isDirectory', cwd: '.'
