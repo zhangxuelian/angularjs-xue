f7f6cc482663a98ee51c4f6ca816d652b0eacf00
zhangxuelian <761303248@qq.com>
2020-03-20 10:42:04 +0800

    feat: zxl 规范sass编写结构，完善button及例子

--- dist/angularjs-xue-full-1.0.0.min.js
+++ dist/angularjs-xue-full-1.0.0.min.js
@@ -2,7 +2,7 @@
  * angularjs-xue
  * Homepage: https://github.com/zhangxuelian/angularjs-xue
  * 
- * Version: 1.0.0 - 2020-03-19
+ * Version: 1.0.0 - 2020-03-20
  * Require angularjs version: 1.2.32
  * License: ISC
  */
