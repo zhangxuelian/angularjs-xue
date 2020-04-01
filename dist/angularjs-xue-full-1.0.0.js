e0e22a1e3a9da0b86df4de17378fa2393fb822f3
chenzechuang <chen1252961354@qq.com>
2020-04-01 12:10:18 +0800

    feat: czc demo完善

--- dist/angularjs-xue-full-1.0.0.js
+++ dist/angularjs-xue-full-1.0.0.js
@@ -2,7 +2,7 @@
  * angularjs-xue
  * Homepage: https://github.com/zhangxuelian/angularjs-xue
  * 
- * Version: 1.0.0 - 2020-03-31
+ * Version: 1.0.0 - 2020-04-01
  * Require angularjs version: 1.2.32
  * License: ISC
  */
